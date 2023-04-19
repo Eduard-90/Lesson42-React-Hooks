85a02b04fbcb2f4e1f31a5d34ad9e07f16da4789
Eduard-90 <eddard1990@gmail.com>
2023-04-19 22:52:52 +0300

    finish lesson 42

--- src/App.js
+++ src/App.js
@@ -1,49 +1,86 @@
 import React, { useState, useEffect } from "react";
 
-  function App () {
-  const [coffe, setCoffe] = useState(0);
-  const [sugar, setSugar] = useState(0);
+function App() {
+  const [products, setProducts] = useState({
+    coffe: 0,
+    sugar: 0,
+  });
 
-const addCoffe = () => setCoffe(coffe + 1);
-const addSugar = () => setSugar(sugar + 1);
+  const addCoffe = () =>
+    setProducts((prevState) => {
+      return {
+        ...prevState,
+        coffe: prevState.coffe + 1,
+      };
+    });
 
-const save = () => {
-  localStorage.setItem('coffe', coffe);
-  localStorage.setItem('sugar', sugar);
-}
+  const removeCoffe = () =>
+    setProducts((prevState) => {
+      return {
+        ...prevState,
+        coffe: prevState.coffe > 0 ? prevState.coffe - 1 : 0,
+      };
+    });
 
-const clear = () => {
-    localStorage.removeItem('coffe');
-    localStorage.removeItem('sugar');
-    setCoffe(0);
-    setSugar(0);
-}
+  const addSugar = () =>
+    setProducts((prevState) => {
+      return {
+        ...prevState,
+        sugar: prevState.sugar + 1,
+      };
+    });
 
-useEffect(()=>{
-  if (localStorage.getItem('coffe')){
-    setCoffe(+localStorage.getItem('coffe'));
-    setSugar(+localStorage.getItem('sugar'));
-  }
-}, []);
+  const removeSugar = () =>
+    setProducts((prevState) => {
+      return {
+        ...prevState,
+        sugar: prevState.sugar > 0 ? prevState.sugar - 1 : 0,
+      };
+    });
 
-return (
-  <div className="wrapper">
-    <div className="list">
-      <h1>Product list</h1>
-      <div className='product'>
-      <span>{`Coffe: ${coffe}`}</span>
-      <button onClick={addCoffe}>Add</button>
-      </div>
-      <div className='product'>
-      <span>{`Sugar: ${sugar}`}</span>
-        <button onClick={addSugar}>Add</button>
-      </div>
-      <div className='save'>
+  const save = () => {
+    localStorage.setItem("coffe", products.coffe);
+    localStorage.setItem("sugar", products.sugar);
+  };
+
+  const clear = () => {
+    localStorage.removeItem("coffe");
+    localStorage.removeItem("sugar");
+    setProducts({
+      coffe: 0,
+      sugar: 0,
+    });
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("coffe")) {
+      setProducts({
+        coffe: +localStorage.getItem("coffe"),
+        sugar: +localStorage.getItem("sugar"),
+      });
+    }
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h1>Product list</h1>
+        <div className="product">
+          <span>{`Coffe: ${products.coffe}`}</span>
+          <button onClick={addCoffe}>Add</button>
+          {products.coffe > 0 && <button onClick={removeCoffe}>Remove</button>}
+        </div>
+        <div className="product">
+          <span>{`Sugar: ${products.sugar}`}</span>
+          <button onClick={addSugar}>Add</button>
+          {products.sugar > 0 && <button onClick={removeSugar}>Remove</button>}
+        </div>
+        <div className="save">
           <button onClick={save}>SAVE</button>
           <button onClick={clear}>CLEAR</button>
         </div>
+      </div>
     </div>
-  </div> 
   );
 }
 
